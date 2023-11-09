c4d1938ce6a1cca519ff32f17696f0690139fbac
Brody <brodywills27@gmail.com>
2023-11-09 11:43:55 -0500

    code quality fixes

--- RNN.py
+++ RNN.py
@@ -8,11 +8,10 @@ max_length = 150
 epochs = 4
 
 # Preprocessing Steps:
-read_data = dataPreprocessing.load_data('data/processed_data.csv')       # load in data
-dataPreprocessing.clean_data(read_data)                                  # clean data
-dataPreprocessing.summarize(read_data)                                   # summarize data
-x_train, x_test, y_train, y_test = dataPreprocessing.split(read_data)    # split data into training and testing sets
-x_train, x_test = dataPreprocessing.tokenize(x_train, x_test, max_features, max_length) # tokenize data
+read_data = dataPreprocessing.load_data('data/processed_data.csv')
+dataPreprocessing.clean_data(read_data)
+x_train, x_test, y_train, y_test = dataPreprocessing.split(read_data)
+x_train, x_test = dataPreprocessing.tokenize(x_train, x_test, max_features, max_length)
 print('Preprocessing complete.')
 
 # create empty model
@@ -24,15 +23,15 @@ model.add(layers.LSTM(100))
 # add dense layer
 model.add(layers.Dense(1, activation='sigmoid'))
 
+# compile model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-
 print("Model Compiled")
 
+# train model
 model.fit(x_train, y_train, epochs=epochs, validation_split=0.2)
-
 print("Model Trained")
 
+# evaluate model
 loss, accuracy = model.evaluate(x_test, y_test)
 print('Accuracy: %f' % (accuracy*100))
 print('Loss: %f' % (loss*100))
-
