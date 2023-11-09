83f92913bb022c82ff29b2058f674307960d5cda
Brody <brodywills27@gmail.com>
2023-11-09 11:42:12 -0500

    fix data preprocessing, complete RNN setup

--- RNN.py
+++ RNN.py
@@ -2,28 +2,35 @@ from keras.models import Sequential
 from keras import layers
 import dataPreprocessing
 
+# Hyperparameters (still needs to be adjusted)
+max_features = 1000
+max_length = 150
+epochs = 4
+
 # Preprocessing Steps:
-read_data = dataPreprocessing.load_data('data/processed_data.csv')
-dataPreprocessing.clean_data(read_data)
-tokenized_data = dataPreprocessing.tokenize(read_data)
-read_data['text'] = tokenized_data
-x_train, x_test, y_train, y_test = dataPreprocessing.split(read_data)
+read_data = dataPreprocessing.load_data('data/processed_data.csv')       # load in data
+dataPreprocessing.clean_data(read_data)                                  # clean data
+dataPreprocessing.summarize(read_data)                                   # summarize data
+x_train, x_test, y_train, y_test = dataPreprocessing.split(read_data)    # split data into training and testing sets
+x_train, x_test = dataPreprocessing.tokenize(x_train, x_test, max_features, max_length) # tokenize data
+print('Preprocessing complete.')
 
 # create empty model
 model = Sequential()
-
-# TODO: Adjust parameters (just using random default values right now)
-
 # add embedding layer
-model.add(layers.Embedding(input_dim=5000, output_dim=50))
+model.add(layers.Embedding(max_features, 100, input_length=max_length))
 # add LSTM layer
-model.add(layers.LSTM(100, dropout=0.2, recurrent_dropout=0.2))
+model.add(layers.LSTM(100))
 # add dense layer
 model.add(layers.Dense(1, activation='sigmoid'))
 
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.fit(x_train, y_train, batch_size=128, epochs=1, validation_split=0.2)
+print("Model Compiled")
+
+model.fit(x_train, y_train, epochs=epochs, validation_split=0.2)
+
+print("Model Trained")
 
 loss, accuracy = model.evaluate(x_test, y_test)
 print('Accuracy: %f' % (accuracy*100))
--- dataPreprocessing.py
+++ dataPreprocessing.py
@@ -1,3 +1,4 @@
+import re
 import pandas as pd
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
@@ -13,21 +14,33 @@ def load_data(path):
 
 def clean_data(data):
     # lowercase all text
-    data['text'] = data['text'].str.lower()
-    # remove punctuation signs, URL's, other symbols
-    data['text'] = data['text'].str.replace(r'[^a-zA-Z0-9\s]', '')
+    data['text'] = data['text'].apply(lambda x: x.lower())
+    # remove usernames tagged in tweets
+    data['text'] = data['text'].apply(lambda x: re.sub('@[^\s]+', '', x))
+    # remove symbols
+    data['text'] = data['text'].apply((lambda x: re.sub('[^a-z0-9\s]', '', x)))
     # remove stop words
     stop = stopwords.words('english')
     data['text'] = data['text'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop)]))
 
 
-def tokenize(data):
-    tokenizer = Tokenizer(num_words=5000)
-    tokenizer.fit_on_texts(data['text'].values)
-    sequences = tokenizer.texts_to_sequences(data['text'].values)
-    text = pad_sequences(sequences, maxlen=50)
-    return text
+def tokenize(x_train, x_test, max_features, max_length):
+    tokenizer = Tokenizer(num_words=max_features)
+    tokenizer.fit_on_texts(x_train)
+    train_sequences = tokenizer.texts_to_sequences(x_train)
+    train = pad_sequences(train_sequences, maxlen=max_length)
+    test_sequences = tokenizer.texts_to_sequences(x_test)
+    test = pad_sequences(test_sequences, maxlen=max_length)
+    return [train, test]
 
 
 def split(data):
-    return train_test_split(data['text'], data['label'], test_size=0.2, random_state=42)
+    return train_test_split(data['text'], data['sentiment'], test_size=0.2, random_state=42)
+
+
+def summarize(data):
+    print('Data Shape: ', data.shape)
+    # print average length of tweets
+    print('Average length of tweet: {}'.format(round(data['text'].str.len().mean())))
+    # print number of positive and negative tweets
+    print('Number of positive/negative tweets: {}'.format(data['sentiment'].value_counts()))
