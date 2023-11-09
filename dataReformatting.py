0db9c96eaf0229f221b06484d04606d8960c8ec9
Brody <brodywills27@gmail.com>
2023-11-09 11:33:37 -0500

    update dataset and dataReformatting.py

--- dataReformatting.py
+++ dataReformatting.py
@@ -1,15 +1,19 @@
 import pandas as pd
 
 # Reformat the original dataset to a new dataset containing only the columns needed
-original_data_path = 'data/training.1600000.processed.noemoticon.csv'
+original_data_path = 'data/Tweets.csv'
 output_data_path = 'data/processed_data.csv'
 
 
 def main():
     # Read in two needed columns of dataset
-    data = pd.read_csv(original_data_path, header=None, encoding='latin-1', usecols=[0, 5], names=['label', 'text'])
-    # Shuffle the rows
-    data = data.sample(frac=0.8)
+    data = pd.read_csv(original_data_path, usecols=[1, 10])
+    # Rename columns
+    data.columns = ['sentiment', 'text']
+    # Remove neutral tweets
+    data = data[data.sentiment != 'neutral']
+    # Convert sentiment to binary
+    data['sentiment'] = data['sentiment'].map({'positive': 1, 'negative': 0})
     # Save the new dataset
     data.to_csv(output_data_path, index=False)
 
